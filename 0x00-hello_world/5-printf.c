651b39b88ae2a2391011d69175c9e7c053bdf4fb
Bekistar <bekistar21@gmail.com>
2023-02-16 10:36:01 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - prints "with proper grammar, but the outcome is 
- *i a piece of art," followed by a new line
+ * a piece of art,"followed by a new line.
  * Return: Always 0.
- *
  */
   int main(void)
 {
