1b9d8ecf722fbd585fbe5fdd365df43c18210922
Bekistar <bekistar21@gmail.com>
2023-02-17 01:40:48 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Seccess)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+
+	return (0);
+}
