174d99a7dc130c42c2659fb6e4afdf63b61171c4
Bekistar <bekistar21@gmail.com>
2023-02-17 02:46:16 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - enrty point
+ * Return: Always 0 (Seccess)
+ */
+  int main(void)
+	  
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+        putchar('\n');
+
+	return (0);
+}
