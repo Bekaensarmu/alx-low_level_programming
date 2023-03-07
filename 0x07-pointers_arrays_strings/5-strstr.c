43af68cf2fa53d5abc748bf33889d21e7d946bcf
Bekistar <bekistar21@gmail.com>
2023-03-07 00:08:19 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -14,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 		char *one = haystack;
 		char *two = needle;
 
-		while (one == *two && *two != '\0')
+		while (*one == *two && *two != '\0')
 		{
 			one++;
 			two++;
