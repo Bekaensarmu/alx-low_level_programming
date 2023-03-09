8a972181767ac14fe426a8ad638739c79c09b77c
Bekistar <bekistar21@gmail.com>
2023-03-08 20:15:49 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,14 +14,14 @@ int _strlen_recursion(char *s)
 }
 /**
  * comparator - compares each character of the string
- * @a: string
+ * @s: string
  * @n1: smallest iterator.
  * @n2: biggest iterator.
  * Return: .
  */
 int comparator(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *(s + n2)
+	if (*(s + n1) == *(s + n2))
 			{
 			if (n1 == n2 ||  n1 == n2 + 1)
 			return (1);
@@ -30,7 +30,7 @@ int comparator(char *s, int n1, int n2)
 			return (0);
 }
 /**
- * is_palindrom - detects if a string is a palindrome
+ * is_palindrome - detects if a string is a palindrome
  * @s: string
  * Return: 1 if s is a palindrome, 0 if not.
  */
@@ -38,5 +38,5 @@ int is_palindrome(char *s)
 {
 if (*s == '\0')
 return (1);
-return (comparatory(s, 0, _strlen_recursion(s) - 1));
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
